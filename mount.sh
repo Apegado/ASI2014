76947f5e5af7deebe9aa02f5c541b457d7a5794e
Victor <vktrsm@gmail.com>
2014-12-19 06:19:14 +0100

    Añadido mas feedback a mount.sh

--- mount.sh
+++ mount.sh
@@ -7,8 +7,9 @@ echo "Configurando Servidor MOUNT"
 nombreDis=`sed '1q;d' $1`
 puntoMontaje=`sed '2q;d' $1`
 
+echo "Intentando montar $nombreDis en $puntoMontaje"
 montaje="$nombreDis     $puntoMontaje   auto    auto    0   0";
 #Copiamos fstab a fstab.tmp
 cat /etc/fstab > fstab.tmp;
 #Comprobamos que el dispositivo no se encuentra montado actualmente si no lo esta lo montamos
-grep -q $nombreDis fstab.tmp && echo "El dispositivo ya esta en fstab" || (echo "$montaje" >> fstab.tmp; mv fstab.tmp /etc/fstab; mount -t auto)
+grep -q $nombreDis fstab.tmp && echo "El dispositivo ya esta en fstab" || (echo "$montaje" >> fstab.tmp && mv fstab.tmp /etc/fstab && mount -t auto && echo "Montaje realizado")
