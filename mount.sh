8827b706e01d2643d4e534c69316d7fe0303c2a7
Victor <vktrsm@gmail.com>
2014-12-19 06:16:52 +0100

    Adaptado mount a nuevo formato

--- mount.sh
+++ mount.sh
@@ -1,45 +1,14 @@
-#! /bin/bash
+#!/bin/bash
 
 echo "============================="
-echo "Configurando Servidor: " $1
-echo "Tipo Servidor: raid"
-echo "Fichero configuracion"  $2
+echo "Configurando Servidor MOUNT"
 
-nombreDis="";
-puntoMontaje="";
+#Cogemos las lineas de manera facil con sed
+nombreDis=`sed '1q;d' $1`
+puntoMontaje=`sed '2q;d' $1`
 
-linea=0;
-
-DONE=false
-until $DONE; do
-    read line || DONE=true
-    #Vemos que servicio es y llamamos a su script
-    case "$linea" in
-        0) nombreDis=$line
-        ;;
-        1) puntoMontaje=$line
-        ;;
-    esac
-    linea=$((linea+1));
-done < $2
-
-echo "Datos Leidos"
-
-echo "$nombreDis";
-echo "$puntoMontaje";
-echo "============================="
 montaje="$nombreDis     $puntoMontaje   auto    auto    0   0";
-echo $montaje;
-
-ssh -oStrictHostKeyChecking=no -t $1 bash -c "'
-
-    echo ""guardamos el fichero en fstab.tmp""
-    cat /etc/fstab > fstab.tmp;
-    echo ""Comprobamos que no este previamente montado.""
-
-    noMontado=true
-    fstab=""/etc/fstab""
-
-    grep -q $nombreDis fstab.tmp && echo ""El dispositivo ya esta en fstab"" || (echo ""Intentamos"" && echo ""$montaje"" >> fstab.tmp && echo "".practic@s"" | sudo -S mv fstab.tmp /etc/fstab;sudo -S mount -t auto)
-
-'"
+#Copiamos fstab a fstab.tmp
+cat /etc/fstab > fstab.tmp;
+#Comprobamos que el dispositivo no se encuentra montado actualmente si no lo esta lo montamos
+grep -q $nombreDis fstab.tmp && echo "El dispositivo ya esta en fstab" || (echo "$montaje" >> fstab.tmp; mv fstab.tmp /etc/fstab; mount -t auto)
