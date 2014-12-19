9c76073e41eb38b670b91a86c114d59880e6ba1b
Victor <vktrsm@gmail.com>
2014-12-19 07:04:39 +0100

    Implementado nfs-client

--- nfs_client.sh
+++ nfs_client.sh
@@ -1,6 +1,18 @@
-#! /bin/bash
+#!/bin/bash
 
 echo "============================="
-echo "Configurando Servidor: " $1
-echo "Tipo Servidor: nfs_client"
-echo "Fichero configuracion"  $2
+echo "Configurando Servidor: NFS CLIENT"
+
+echo "Instalando nfs-common...."
+apt-get install nfs-common -qq --force-yes > /dev/null
+
+while read line || [ -n "$line" ]
+do 
+  $remoteDir=$(echo $line | awk '{print $1}')
+  $localDir=$(echo $line | awk '{print $2}')
+  echo "Montando $remoteDir en $localDir"
+  echo "$remoteDir     $localDir   nfs    rsize=8192,wsize=8192,timeo=14,intr" >> /etc/fstab
+done <$1
+
+echo "Recargando fstab..."
+mount -t auto
