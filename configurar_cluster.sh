b3a5c855ce91c0dd46e1a2af3f75191326e44622
Victor <vktrsm@gmail.com>
2014-12-19 07:13:37 +0100

    Mejor lectura de parametros usando awk para tener trimming e ignorar espacios posible

--- configurar_cluster.sh
+++ configurar_cluster.sh
@@ -8,26 +8,17 @@ while read line || [ -n "$line" ]
 do 
   case "$line" in \#*) continue ;; esac #Saltarse comentarios
   
-  nWord=0
-  for word in $line
-  do
-    case "$nWord" in
-      0) remoteAddress=$word
-      ;;
-
-      1) serviceName=$word
-      ;;
-
-      2) serviceFile=$word
-         echo "Configurando servicio $serviceName en maquina $remoteAddress con fichero de configuracion $serviceFile..."
-         echo "Copiando ficheros $serviceFile y $serviceName.sh ..."
+  #Lectura de parametros de linea (mejor usar awk para hacer funcion de trimming)
+  remoteAddress=$(echo $line | awk '{print $1}')
+  serviceName=$(echo $line | awk '{print $2}')
+  serviceFile=$(echo $line | awk '{print $3}')
+  
+  echo "Configurando servicio $serviceName en maquina $remoteAddress con fichero de configuracion $serviceFile..."
+  echo "Copiando ficheros $serviceFile y $serviceName.sh ..."
          
-         scp $serviceFile root@$remoteAddress:  #Copiamos el fichero de configuracion a la maquina remoto
-         scp ./$serviceName.sh root@$remoteAddress: #Copiamos el script a la maquina remota
-         #Ejecutamos el script (solo con el basename del fichero por si la ruta era absoluta) y borramos los ficheros
-         ssh -n -oStrictHostKeyChecking=no root@$remoteAddress "./$serviceName.sh `basename "$serviceFile"`; rm $serviceName.sh; rm `basename "$serviceFile"`" 
-      ;;
-    esac
-    ((nWord++))
-  done
+  scp $serviceFile root@$remoteAddress:  #Copiamos el fichero de configuracion a la maquina remoto
+  scp ./$serviceName.sh root@$remoteAddress: #Copiamos el script a la maquina remota
+  #Ejecutamos el script (solo con el basename del fichero por si la ruta era absoluta) y borramos los ficheros
+  ssh -n -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null root@$remoteAddress "./$serviceName.sh `basename "$serviceFile"`; rm $serviceName.sh; rm `basename "$serviceFile"`"
+  
 done < $1
