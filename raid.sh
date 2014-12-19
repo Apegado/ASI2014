a21a6e1e6bf8535826b9e37181fc72ee093a81ea
Victor <vktrsm@gmail.com>
2014-12-19 06:23:54 +0100

    Adaptado raid a nuevo formato

--- raid.sh
+++ raid.sh
@@ -1,49 +1,20 @@
 #! /bin/bash
 
 echo "============================="
-echo "Configurando Servidor: " $1
-echo "Tipo Servidor: raid"
-echo "Fichero configuracion"  $2
-echo "============================="
+echo "Configurando Servidor RAID"
 
-nombreDis="";
-nivelRaid=-1;
-dispositivos="";
+#Cogemos las lineas de manera facil con sed
+nombreDis=`sed '1q;d' $1`
+nivelRaid=`sed '2q;d' $1`
+dispositivos=`sed '3q;d' $1`
 
-linea=0;
-
-DONE=false
-until $DONE; do
-    read line || DONE=true
-    #Vemos que servicio es y llamamos a su script
-    case "$linea" in
-        0) nombreDis=$line
-        ;;
-        1) nivelRaid=$line
-        ;;
-        2) dispositivos=$line
-        ;;
-    esac
-    linea=$((linea+1));
-done < $2
-
-
+#Calculamos el numero de dispositivos
 ndispositivos=$(echo "$dispositivos" | wc -w)
 
-echo "Datos Leidos:"
-
-echo "$nombreDis";
-echo "$nivelRaid";
-echo "$dispositivos";
-echo "$ndispositivos"
-
-
 echo "============================="
-ssh -oStrictHostKeyChecking=no -t $1 bash -c "'
 
-    echo ""Instalando mdadm""
-    echo "".practic@s"" | sudo -S apt-get install -y mdadm >> /dev/null
-    echo "" Creando raid $nivelRaid devices $dispositivos""
-    echo "".practic@s"" | sudo -S mdadm --create --level=$nivelRaid --raid-devices=$ndispositivos $nombreDis -R $dispositivos
+echo "Instalando mdadm..."
+apt-get install mdadm -qq --force-yes > /dev/null
 
-    '"
+echo "Creando raid $nivelRaid devices $dispositivos"
+mdadm --create --level=$nivelRaid --raid-devices=$ndispositivos $nombreDis -R $dispositivos
